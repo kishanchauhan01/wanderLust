e3479206bf84a46d818bc57c185665037f136915
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2025-05-03 14:16:14 +0530

    add all route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const connectDB = require("./connectDB.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 connectDB()
   .then(() => {
@@ -16,6 +17,7 @@ connectDB()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("hi hello");
@@ -27,20 +29,47 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 });
 
-// New route
+// New route (because if we write below the show route then express assume new as :id)
 app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs")
-})
-
+  res.render("listings/new.ejs");
+});
 
 //Show route
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", {listing});
+  res.render("listings/show.ejs", { listing });
 });
 
+//Create Route
+app.post("/listings", async (req, res) => {
+  // let {title, descripttion, image, price, country, location} = req.body;
+  let newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedLising = await Listing.findByIdAndDelete(id);
+  console.log(deletedLising);
+  res.redirect("/listings");
+});
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
