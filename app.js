8d7729a68a8a6227e46c7b42007241d63b62ddad
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2025-05-04 12:56:40 +0530

    add styling to the page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const connectDB = require("./connectDB.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 connectDB()
   .then(() => {
@@ -18,6 +19,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("hi hello");
